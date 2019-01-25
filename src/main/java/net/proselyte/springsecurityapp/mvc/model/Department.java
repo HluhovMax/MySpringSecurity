b415e77de12e2c4f8c2e319c97502ab7b5ab573e
Hluhov Max <holosievo@gmail.com>
2019-01-25 12:55:42 +0200

    all updated

--- src/main/java/net/proselyte/springsecurityapp/mvc/model/Department.java
+++ src/main/java/net/proselyte/springsecurityapp/mvc/model/Department.java
@@ -2,16 +2,14 @@ package net.proselyte.springsecurityapp.mvc.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
-import java.util.Objects;
 import java.util.Set;
 
 /**
  * Created by 38066 on 24.01.2019.
  */
+@Data
 @Entity
 @Table(name = "department")
 public class Department {
@@ -21,56 +19,7 @@ public class Department {
     private int id;
     @Column(name = "name")
     private String name;
+    @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "department", targetEntity = Employee.class)
     private Set<Employee> employees;
-
-    public Department() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Employee> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(Set<Employee> employees) {
-        this.employees = employees;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Department that = (Department) o;
-        return id == that.id &&
-                Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", employees=" + employees +
-                '}';
-    }
 }
--- src/main/java/net/proselyte/springsecurityapp/mvc/model/Employee.java
+++ src/main/java/net/proselyte/springsecurityapp/mvc/model/Employee.java
@@ -1,6 +1,7 @@
 package net.proselyte.springsecurityapp.mvc.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.sql.Date;
@@ -9,7 +10,7 @@ import java.util.Objects;
 /**
  * Created by 38066 on 24.01.2019.
  */
-
+@Data
 @Entity
 @Table(name = "employee")
 public class Employee {
@@ -25,77 +26,10 @@ public class Employee {
     private Date birthday;
     @Column(name = "hire_date")
     private Date hire;
+    @EqualsAndHashCode.Exclude
     @ManyToOne
     @JoinColumn(name = "department_id")
     private Department department;
-
-    public Employee() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public Date getHire() {
-        return hire;
-    }
-
-    public void setHire(Date hire) {
-        this.hire = hire;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return id == employee.id &&
-                salary == employee.salary &&
-                Objects.equals(name, employee.name) &&
-                Objects.equals(birthday, employee.birthday) &&
-                Objects.equals(hire, employee.hire);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, salary, birthday, hire);
-    }
 
     @Override
     public String toString() {
